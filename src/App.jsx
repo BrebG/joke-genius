28afa8abfbd0fc44edf0682f19fc2082e6d40a44
BrebG <brebelgael@protonmail.com>
2024-05-13 15:10:30 +0200

    nice pull

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,38 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-	const [setup, setSetup] = useState();
+  const [setup, setSetup] = useState();
 
-	async function getSetup() {
-		await fetch('https://official-joke-api.appspot.com/random_joke')
-			.then((response) => response.json())
-			.then((response) => {
-				setSetup(response);
-			})
-			.catch((err) => console.error(err));
-	}
+  async function getSetup() {
+    await fetch('https://official-joke-api.appspot.com/random_joke')
+      .then((response) => response.json())
+      .then((response) => {
+        setSetup(response);
+      })
+      .catch((err) => console.error(err));
+  }
 
-	const DelayedMessage = () => {
-		const [message, setMessage] = useState('');
+  const DelayedMessage = () => {
+    const [message, setMessage] = useState('');
 
-		useEffect(() => {
-			const timeoutId = setTimeout(() => {
-				setMessage(`${setup?.punchline}`);
-			}, 2000);
-			return () => clearTimeout(timeoutId);
-		}, []);
+    useEffect(() => {
+      const timeoutId = setTimeout(() => {
+        setMessage(`${setup?.punchline}`);
+      }, 2000);
+      return () => clearTimeout(timeoutId);
+    }, []);
 
-		return <div>{setup == undefined ? <h2></h2> : <h2>{message}</h2>}</div>;
-	};
+    return <div>{setup == undefined ? <h2></h2> : <h2>{message}</h2>}</div>;
+  };
 
-	return (
-		<>
-			<h1>Joke Genius</h1>
-			<h2>{setup?.setup}</h2>
-			<DelayedMessage />
-			<button onClick={getSetup}>Get joke</button>
-		</>
-	);
+  return (
+    <>
+      <h1>Joke Genius</h1>
+      <h2>{setup?.setup}</h2>
+      <DelayedMessage />
+      <button onClick={getSetup}>Get joke</button>
+    </>
+  );
 }
 
 export default App;
