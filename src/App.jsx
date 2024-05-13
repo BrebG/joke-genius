f751db8ebba743cb9dcd4c07b819de04a8114cca
ileanaspl <ileana.saintpaul@gmail.com>
2024-05-13 14:19:58 +0200

    nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [setup, setSetup] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	async function getSetup() {
+		await fetch('https://official-joke-api.appspot.com/random_joke')
+			.then((response) => response.json())
+			.then((response) => {
+				setSetup(response);
+			})
+			.catch((err) => console.error(err));
+	}
+
+	const DelayedMessage = () => {
+		const [message, setMessage] = useState('');
+
+		useEffect(() => {
+			const timeoutId = setTimeout(() => {
+				setMessage(`${setup?.punchline}`);
+			}, 2000);
+			return () => clearTimeout(timeoutId);
+		}, []);
+
+		return <div>{setup == undefined ? <h2></h2> : <h2>{message}</h2>}</div>;
+	};
+
+	return (
+		<>
+			<h1>Joke Genius</h1>
+			<h2>{setup?.setup}</h2>
+			<DelayedMessage />
+			<button onClick={getSetup}>Get joke</button>
+		</>
+	);
 }
 
-export default App
+export default App;
